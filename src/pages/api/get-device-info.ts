d27aed336abb2b74e3bb9b1684def82fc0c27a3a
kostassagma <kostassagmatopoulos@gmail.com>
2021-11-11 20:50:00 +0200

    trying to get user ip

--- src/pages/api/get-device-info.ts
+++ src/pages/api/get-device-info.ts
@@ -1,8 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+import requestIp from 'request-ip'
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const headers = req.headers;
+  const detectedIp = requestIp.getClientIp(req)
 
-  res.status(200).json({ ...headers });
+  res.status(200).json({ detectedIp  });
 }
